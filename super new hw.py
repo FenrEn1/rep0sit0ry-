eab93dfcc12ca47d9334c3732abde6ecee5cd850
FenrEn1 <56796797+FenrEn1@users.noreply.github.com>
2019-10-27 15:26:40 +0300

    Add files via upload

---
+++ super new hw.py
@@ -0,0 +1,14 @@
+my_list = []
+while True:
+    input("Введите имя")
+    if input = "add" + input: # ( my_list, [] )
+        my_list_append() #Добавить в список
+    elif input = "remove" + input: #ввели уже написанное имя
+        my_list_remove() #Убрать из списка
+    elif input = : #Если имя уже было было введено
+         print("Нельзя вводить одно имя два или более раз!")
+    elif input = "team":
+         print(my_list)
+    
+
+
